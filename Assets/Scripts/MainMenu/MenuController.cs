8c8ac016bf752cd58d329fe7aff3fb55a67f0cdd
Jonathan Hecl <jonathanhecl@gmail.com>
2023-06-19 15:21:14 -0300

    Credits on menu

--- Assets/Scripts/MainMenu/MenuController.cs
+++ Assets/Scripts/MainMenu/MenuController.cs
@@ -6,12 +6,18 @@ using UnityEngine.UI;
 public class MenuController : MonoBehaviour
 {
     [SerializeField] private Button m_playButton;
+    [SerializeField] private Button m_creditButton;
     [SerializeField] private Button m_quitButton;
+    [SerializeField] private Button m_returnButton;
+    [SerializeField] private GameObject m_panelMain;
+    [SerializeField] private GameObject m_panelCredits;
 
     private void Awake()
     {
         m_playButton.onClick.AddListener(PlayGame);
+        m_creditButton.onClick.AddListener(GoToCredits);
         m_quitButton.onClick.AddListener(QuitGame);
+        m_returnButton.onClick.AddListener(GoToMain);
     }
 
 
@@ -24,4 +30,18 @@ public class MenuController : MonoBehaviour
     {
         Application.Quit();
     }
+
+
+    public void GoToCredits()
+    {
+        m_panelCredits.SetActive(true);
+        m_panelMain.SetActive(false);
+    }
+
+    public void GoToMain()
+    {
+        m_panelCredits.SetActive(false);
+        m_panelMain.SetActive(true);
+    }
+
 }
